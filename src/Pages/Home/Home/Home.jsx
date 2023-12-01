52a5337694eaa39b2346e5ae57248a6b184de1ec
tanjib10 <radiulmd@gmail.com>
2023-12-01 13:40:42 +0600

    basic setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Athletic Zone | Home</title>
+      </Helmet>
+      <h3>Home</h3>
+    </div>
+  );
+};
+
+export default Home;
