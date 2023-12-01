c8a7c2ef9b1bcec424908d64708050ea3013f917
tanjib10 <radiulmd@gmail.com>
2023-12-01 13:59:54 +0600

    added footer

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
