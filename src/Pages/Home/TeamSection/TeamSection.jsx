00b9bc9dfdd980e355c2c441d95c59cb71abb7fd
tanjib10 <radiulmd@gmail.com>
2023-12-02 20:41:10 +0600

    updated team section

--- src/Pages/Home/TeamSection/TeamSection.jsx
+++ src/Pages/Home/TeamSection/TeamSection.jsx
@@ -1,34 +1,12 @@
-const TeamSection = () => {
-  const teamMembers = [
-    {
-      id: 1,
-      name: "John Doe",
-      position: "Head Coach",
-      description:
-        "With over a decade of experience, John is dedicated to helping you achieve your fitness goals. His holistic approach to coaching combines expertise in strength training, nutrition, and motivation.",
-      imageSrc:
-        "https://i.ibb.co/6s90fgD/michael-dam-m-EZ3-Po-FGs-k-unsplash.jpg",
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      position: "Fitness Trainer",
-      description:
-        "Jane is passionate about guiding individuals towards a healthier lifestyle. Her dynamic fitness routines and personalized training plans ensure that each session is challenging and enjoyable.",
-      imageSrc:
-        "https://i.ibb.co/LJyNSCJ/alex-suprun-ZHv-M3-XIOHo-E-unsplash.jpg",
-    },
-    {
-      id: 3,
-      name: "Bob Johnson",
-      position: "Yoga Instructor",
-      description:
-        "Discover the transformative power of yoga with Bob. As a certified yoga instructor, Bob brings a blend of mindfulness and physical wellness to his classes, helping you find balance and peace.",
-      imageSrc:
-        "https://i.ibb.co/jb2rJSv/alexander-hipp-i-EEBWg-Y-6l-A-unsplash.jpg",
-    },
-  ];
+import { useEffect, useState } from "react";
 
+const TeamSection = () => {
+  const [teamMembers, setTeamMembers] = useState([]);
+  useEffect(() => {
+    fetch("/team.json")
+      .then((res) => res.json())
+      .then((data) => setTeamMembers(data));
+  }, []);
   return (
     <section className="py-16 my-16 bg-gradient-to-r from-[#f5f5f5] to-[#ffffff]">
       <div className="container mx-auto">
@@ -54,7 +32,7 @@ const TeamSection = () => {
                 {teamMember.name}
               </h3>
               <p className="text-gray-700 mb-4">{teamMember.position}</p>
-              <p className="text-gray-600">{teamMember.description}</p>
+              <p className="text-[#31304D]">{teamMember.description}</p>
             </div>
           ))}
         </div>
