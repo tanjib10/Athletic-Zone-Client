37f304ca91f556a7ebbf2ba785eb1305bd797c35
tanjib10 <radiulmd@gmail.com>
2023-12-19 16:13:20 +0600

    added animation on the trainer page"

--- src/Pages/Trainers/Trainers.jsx
+++ src/Pages/Trainers/Trainers.jsx
@@ -2,9 +2,12 @@ import { FaFacebook, FaTwitter } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { motion, useAnimation } from "framer-motion";
 
 const Trainers = () => {
   const [trainers, setTrainers] = useState([]);
+  const sectionControls = useAnimation();
+  const trainersControls = useAnimation();
 
   useEffect(() => {
     const fetchTrainers = async () => {
@@ -14,73 +17,150 @@ const Trainers = () => {
         );
         const data = await response.json();
         setTrainers(data);
+        sectionControls.start({
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.8 },
+        });
+        trainersControls.start({
+          opacity: 1,
+          transition: { staggerChildren: 0.2 },
+        });
       } catch (error) {
         console.error("Error fetching trainers:", error);
       }
     };
 
     fetchTrainers();
-  }, []);
+  }, [sectionControls, trainersControls]);
 
   return (
-    <section className="py-16 bg-gray-100">
+    <motion.section
+      className="py-16"
+      initial={{ opacity: 0, y: 20 }}
+      animate={sectionControls}
+    >
       <Helmet>
         <title>Athletic Zone | Trainers</title>
       </Helmet>
       <div className="container mx-auto">
-        <h2 className="text-4xl font-bold mb-8 text-center text-[#161A30]">
+        <motion.h2
+          className="text-4xl font-bold mb-8 text-center text-[#161A30]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           Meet Our Trainers
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        </motion.h2>
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+          initial={{ opacity: 0 }}
+          animate={trainersControls}
+        >
           {trainers.map((trainer) => (
-            <div
+            <motion.div
               key={trainer._id}
               className="bg-white p-6 rounded-lg shadow-md"
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 },
+              }}
             >
-              <img
+              <motion.img
                 src={trainer.profileImage}
                 alt={trainer.name}
                 className="w-full h-60 object-cover mb-4 rounded-md"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
               />
-              <h3 className="text-lg font-semibold mb-2">{trainer.name}</h3>
-              <p className="text-gray-600 mb-2">
+              <motion.h3
+                className="text-lg font-semibold mb-2"
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
+                {trainer.name}
+              </motion.h3>
+              <motion.p
+                className="text-gray-600 mb-2"
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
                 Years of Experience: {trainer.experience}
-              </p>
-              <p className="text-gray-600 mb-2">
+              </motion.p>
+              <motion.p
+                className="text-gray-600 mb-2"
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
                 Available Slots: {trainer.availableSlots}
-              </p>
-              <div className="mb-4">
-                <span className="flex">
+              </motion.p>
+              <motion.div
+                className="mb-4"
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
+                <motion.span className="flex">
                   {" "}
                   <FaTwitter className="text-2xl text-blue-400 mr-2 cursor-pointer"></FaTwitter>
                   <FaFacebook className="text-2xl text-blue-700 cursor-pointer"></FaFacebook>
-                </span>
-              </div>
-              <Link to={`/trainer/${trainer._id}`}>
-                <button className="btn bg-[#31304D] text-white py-2 px-4 rounded-md hover:bg-[#31304D] transition duration-300">
+                </motion.span>
+              </motion.div>
+              <Link
+                to={`/trainer/${trainer._id}`}
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
+                <motion.button className="btn bg-[#31304D] text-white py-2 px-4 rounded-md hover:bg-[#31304D] transition duration-300">
                   Know More
-                </button>
+                </motion.button>
               </Link>
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
-      <div className="text-center py-16">
-        <h2 className="text-3xl font-bold mb-4 text-[#161A30]">
+      <motion.div
+        className="text-center py-16"
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <motion.h2
+          className="text-3xl font-bold mb-4 text-[#161A30]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           Ready to Inspire Others?
-        </h2>
-        <p className="text-gray-600 mb-6">
+        </motion.h2>
+        <motion.p
+          className="text-gray-600 mb-6"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           Join our team of passionate trainers and help others achieve their
           fitness goals.
-        </p>
+        </motion.p>
         <Link
           to="/becomeTrainer"
           className="btn w-1/4 h-24  bg-[#F76D57] text-xl font-bold text-white  px-6 rounded-lg lg:rounded-full hover:bg-[#B6BBC4] transition duration-300"
         >
           Become a Trainer
         </Link>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 };
 
